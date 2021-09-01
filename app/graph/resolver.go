717ff674e80011b6f1c222f785e3ba2bb7363066
YusukeMatsukawa <matsukawa@grabss.co.jp>
2021-09-01 11:28:39 +0900

    Feat: todosサンプル実装

--- app/graph/resolver.go
+++ app/graph/resolver.go
@@ -4,4 +4,10 @@ package graph
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+import (
+	"go-gqlgen-template/graph/model"
+)
+
+type Resolver struct {
+	todos []*model.Todo
+}
